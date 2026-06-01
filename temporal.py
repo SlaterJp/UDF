903faaebbe612a7ff902b4ffaa1d9431f1046a55
Paulo Miranda <mirandajp.oliver@gmail.com>
2026-06-01 18:43:50 -0300

    third commit

--- temporal.py
+++ temporal.py
@@ -1,10 +1,19 @@
-from google.colab import drive
-drive.mount('/content/drive')
 from pandas import read_csv
 from matplotlib import pyplot
 
-# Carrega o arquivo do seu link do Drive e processa os dados em uma única linha
-series = read_csv(r"/content/drive/MyDrive/caso_full.csv.gz", index_col='date', parse_dates=True).query("place_type == 'state'").groupby('date')['new_confirmed'].sum()
+url = "https://raw.githubusercontent.com/SlaterJp/UDF/main/caso_full.csv.gz"
 
-series.plot() 
+series = (
+    read_csv(
+        url,
+        index_col='date',
+        parse_dates=True,
+        compression='gzip'
+    )
+    .query("place_type == 'state'")
+    .groupby('date')['new_confirmed']
+    .sum()
+)
+
+series.plot()
 pyplot.show()
