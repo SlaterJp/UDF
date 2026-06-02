94ab0b744bc1ea565c51b496c3f894cdae27fd8c
Paulo Miranda <mirandajp.oliver@gmail.com>
2026-06-01 21:11:37 -0300

    commit2

--- temporal.py
+++ temporal.py
@@ -3,17 +3,17 @@ from matplotlib import pyplot
 
 url = "https://raw.githubusercontent.com/SlaterJp/UDF/main/WHO-COVID-19-global-daily-data.csv"
 
-series = (
-    read_csv(
-        url,
-        index_col='date',
-        parse_dates=True,
-        compression='gzip'
-    )
-    .query("place_type == 'state'")
-    .groupby('date')['new_confirmed']
-    .sum()
-)
+df = read_csv(url, parse_dates=['Date_reported'])
 
-series.plot()
+df.set_index('Date_reported', inplace=True)
+
+series_mensal = df['New_cases'].resample('ME').sum()
+
+series_mensal.plot(figsize=(12, 6), marker='o', color='#1f77b4', linewidth=2)
+
+pyplot.title("Evolução Temporal: Total de Novos Casos Globais de COVID-19 por Mês")
+pyplot.xlabel("Intervalo Temporal (Ano-Mês)")
+pyplot.ylabel("Total de Casos no Mês")
+pyplot.grid(True, linestyle='--', alpha=0.5)
+
 pyplot.show()
